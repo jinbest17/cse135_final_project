7c4bcafacfc6604a97ec4e2eb82f78cf308119f4
Jinhao <jinhaoliu18@hotmail.com>
2020-04-28 14:47:26 -0700

    finished connecting to db and adding routers structure

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-
+const mongoose = require('mongoose')
+require("dotenv/config")
 const app = express();
 
-// routes
-app.get('/', (req, res) => {
-    res.send("Hello World");
+// import routes
+const routes = require('./routes');
+
+app.use('/', routes);
+// db connection
+mongoose.connect(process.env.MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to db");
+}).catch((error) => {
+    console.log(error);
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 
-// post router for apis here
+// add router for apis here
+router.get('/', (req, res) => {
+    res.send("You are on home page");
+});
 
+router.get('/posts', (req, res) => {
+    res.send("You are on posts page");
+});
 
 module.exports = router;
